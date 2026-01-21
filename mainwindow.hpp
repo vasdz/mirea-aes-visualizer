431f9d5e2e6b38fe6118edc80f5cd7251781514b
Alexander <103282255+vasdz@users.noreply.github.com>
2026-01-21 21:46:43 +0300

    Update mainwindow.hpp

--- mainwindow.hpp
+++ mainwindow.hpp
@@ -3,7 +3,9 @@
 #include <QMainWindow>
 #include <array>
 #include <vector>
-#include <QString>
+#include <cstdint>
+#include <span>
+#include <QCloseEvent> 
 
 class QTableWidget;
 class QLineEdit;
@@ -14,56 +16,54 @@ class QProgressBar;
 
 class MainWindow : public QMainWindow {
 Q_OBJECT
-
 public:
-explicit MainWindow(QWidget *parent = nullptr);
-~MainWindow() override = default;
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override = default;
 
 protected:
-void closeEvent(QCloseEvent *event) override;
+    void closeEvent(QCloseEvent *event) override;
 
 private slots:
-void onEncryptClicked();
-void onNextStep();
-void onPrevStep();
+    void onEncryptClicked();
+    void onNextStep();
+    void onPrevStep();
 
 private:
-using State = std::array<std::array<uint8_t, 4>, 4>;
+    using State = std::array<std::array<uint8_t, 4>, 4>;
+    struct Step {
+        QString name;
+        State state;
+        QString description;
+    };
 
-struct Step {
-QString name;
-State state;
-QString description;
-};
+    void applyDarkTheme();
+    void setupUi();
+    void generateRandomKey();
 
-void applyDarkTheme();
-void setupUi();
-void generateRandomKey();
+    // AES Core Functions
+    void keyExpansion(const std::vector<uint8_t>& key, std::vector<uint8_t>& roundKeys);
+    void subBytes(State& state);
+    void shiftRows(State& state);
+    void mixColumns(State& state);
+    void addRoundKey(State& state, const std::vector<uint8_t>& roundKeys, int round);
 
-// AES Core Functions
-void keyExpansion(const std::vector<uint8_t>& key, std::vector<uint8_t>& roundKeys);
-void subBytes(State& state);
-void shiftRows(State& state);
-void mixColumns(State& state);
-void addRoundKey(State& state, const std::vector<uint8_t>& roundKeys, int round);
+    void setupEncryptPage(QWidget* page);
+    void setupTheoryPage(QWidget* page);
+    void setupModesPage(QWidget* page);
 
-void setupEncryptPage(QWidget* page);
-void setupTheoryPage(QWidget* page);
-void setupModesPage(QWidget* page);
+    void updateStateView();
+    void updateTheoryView();
 
-void updateStateView();
-void updateTheoryView();
+    QTableWidget* stateTable = nullptr;
+    QLineEdit* plaintextEdit = nullptr;
+    QLineEdit* keyEdit = nullptr;
+    QComboBox* modeBox = nullptr;
+    QProgressBar* progressBar = nullptr;
+    QPushButton* encryptButton = nullptr;
+    QPushButton* nextStepButton = nullptr;
+    QPushButton* prevStepButton = nullptr;
+    QTextBrowser* theoryView = nullptr;
 
-QTableWidget* stateTable = nullptr;
-QLineEdit* plaintextEdit = nullptr;
-QLineEdit* keyEdit = nullptr;
-QComboBox* modeBox = nullptr;
-QProgressBar* progressBar = nullptr;
-QPushButton* encryptButton = nullptr;
-QPushButton* nextStepButton = nullptr;
-QPushButton* prevStepButton = nullptr;
-QTextBrowser* theoryView = nullptr;
-
-std::vector<Step> steps;
-int currentStepIndex = -1;
+    std::vector<Step> steps;
+    int currentStepIndex = -1;
 };
